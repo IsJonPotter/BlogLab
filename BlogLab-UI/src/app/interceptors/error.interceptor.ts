66ad7052b1046538f09cb585e0282505d5b9c807
Jonathan Potter <isjonpotter@gmail.com>
2022-01-04 10:13:15 +0000

    Main app created Navbar component and routing created, plus some basic fixes.

--- BlogLab-UI/src/app/interceptors/error.interceptor.ts
+++ BlogLab-UI/src/app/interceptors/error.interceptor.ts
@@ -8,11 +8,13 @@ import {
 import { catchError, Observable, throwError } from 'rxjs';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../services/account.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
   constructor(
     private toastr: ToastrService,
+    private router: Router,
     private accountService: AccountService
   ) {}
 
@@ -56,7 +58,7 @@ export class ErrorInterceptor implements HttpInterceptor {
       this.toastr.error(errorMessage, error.statusText);
       console.log(error.error);
     } else if (
-      !!error?.error.errors?.Content &&
+      !!error?.error?.errors?.Content &&
       typeof error.error.errors.Content == 'object'
     ) {
       let errorObject = error.error.errors.Content;
@@ -69,7 +71,7 @@ export class ErrorInterceptor implements HttpInterceptor {
       console.log(error.error);
     } else if (!!error.error) {
       let errorMessage =
-        typeof error.error == 'string'
+        typeof error.error === 'string'
           ? error.error
           : 'There was a validation error.';
       this.toastr.error(errorMessage, error.statusCode);
@@ -84,7 +86,7 @@ export class ErrorInterceptor implements HttpInterceptor {
     let errorMessage = 'Please login to your account.';
     this.accountService.logout();
     this.toastr.error(errorMessage, error.statusText);
-    // route to the login page
+    this.router.navigate(['/login']);
   }
 
   handle500Error(error: any) {
